0723f1a823f866cdb8026913a1e107dc591fd838
Oleksii Pudlo <pudloaleksey@gmail>
2019-02-13 16:06:09 +0100

    Updates

---
+++ precache-manifest.9ae5e06f1c924e9f6257b0de3639a925.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "a87a3a7a3e3808869077",
+    "url": "/todolist/static/js/main.a87a3a7a.chunk.js"
+  },
+  {
+    "revision": "53f1e0702107373675d6",
+    "url": "/todolist/static/js/1.53f1e070.chunk.js"
+  },
+  {
+    "revision": "a87a3a7a3e3808869077",
+    "url": "/todolist/static/css/main.18e0b0c3.chunk.css"
+  },
+  {
+    "revision": "1baf706fb3c001cc093067e4e26c5449",
+    "url": "/todolist/index.html"
+  }
+];
