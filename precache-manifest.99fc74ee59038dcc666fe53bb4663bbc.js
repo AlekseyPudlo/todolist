628344431a75b002677e665f2cb8f87bf0405769
Oleksii Pudlo <pudloaleksey@gmail>
2019-02-26 21:19:21 +0100

    Updates

--- precache-manifest.99fc74ee59038dcc666fe53bb4663bbc.js
+++ precache-manifest.99fc74ee59038dcc666fe53bb4663bbc.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/todolist/static/js/runtime~main.f2982e14.js"
   },
   {
-    "revision": "ff2aff7278b5f2158ceb",
-    "url": "/todolist/static/js/main.ff2aff72.chunk.js"
+    "revision": "3bdf27149d95c25c8ed2",
+    "url": "/todolist/static/js/main.3bdf2714.chunk.js"
   },
   {
     "revision": "53f1e0702107373675d6",
     "url": "/todolist/static/js/1.53f1e070.chunk.js"
   },
   {
-    "revision": "ff2aff7278b5f2158ceb",
-    "url": "/todolist/static/css/main.930d7da1.chunk.css"
+    "revision": "3bdf27149d95c25c8ed2",
+    "url": "/todolist/static/css/main.a392bb12.chunk.css"
   },
   {
-    "revision": "8a361d5994986999277cc7d4fcf6b250",
+    "revision": "c63dacfa631cf1b90eb2e185cd017276",
     "url": "/todolist/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todolist/precache-manifest.8d11e03c748a510f8eaba1347c21a533.js"
+  "/todolist/precache-manifest.99fc74ee59038dcc666fe53bb4663bbc.js"
 );
 
 workbox.clientsClaim();
